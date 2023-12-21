ef18f84318dda497bbb89dec49bc769372e8af69
Kirill Lysin <lysinkirill@gmail.com>
2023-12-21 17:46:19 +0300

    Add access methods declaration to SessionDao; Rename MovieSessionEntity.kt to SessionEntity.kt

--- src/main/kotlin/data/SessionDao.kt
+++ src/main/kotlin/data/SessionDao.kt
@@ -1,4 +1,10 @@
 package data
 
+import domain.entitiy.SessionEntity
+
 interface SessionDao {
+    fun add(session: SessionEntity)
+    fun get(sessionId: Int) : SessionEntity?
+    fun getAll() : List<SessionEntity>
+    fun update(vararg listAccount: SessionEntity)
 }
--- src/main/kotlin/domain/entitiy/SessionEntity.kt
+++ src/main/kotlin/domain/entitiy/SessionEntity.kt
@@ -3,7 +3,7 @@ package domain.entitiy
 import kotlinx.datetime.LocalDateTime
 
 
-data class MovieSessionEntity(
+data class SessionEntity(
     val filmId: Int,
     val startTime: LocalDateTime,
     val soldTicketIds: List<Int> = listOf(),
