ab5d45dbf3fe80ef6b1ebfb9d50d204ba18b3d4c
Kirill Lysin <lysinkirill@gmail.com>
2023-12-22 05:50:36 +0300

    Add new SessionDao implementation - RuntimeSessionDao

---
+++ src/main/kotlin/data/RuntimeSessionDao.kt
@@ -0,0 +1,24 @@
+package data
+
+import domain.entitiy.SessionEntity
+
+class RuntimeSessionDao : SessionDao {
+    val sessions = mutableMapOf<Int, SessionEntity>()
+    override fun addSession(session: SessionEntity) {
+        sessions[session.sessionId] = session
+    }
+
+    override fun getSession(sessionId: Int): SessionEntity? {
+        return sessions[sessionId]
+    }
+
+    override fun getAllSession(): List<SessionEntity> {
+        return sessions.values.toList()
+    }
+
+    override fun updateWithSessions(vararg listAccount: SessionEntity) {
+        for(session in listAccount) {
+            sessions[session.sessionId] = session
+        }
+    }
+}
